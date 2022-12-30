0bed67af7b27438f23f2017bb6b4feafdd4b3bf8
Dominic Ndondo <59946287+Dominic263@users.noreply.github.com>
2022-12-30 14:45:40 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# customizableAnimations
+
+A description of this package.
---
+++ Tests/customizableAnimationsTests/customizableAnimationsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import customizableAnimations
+
+final class customizableAnimationsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(customizableAnimations().text, "Hello, World!")
+    }
+}
